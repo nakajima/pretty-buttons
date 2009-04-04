f856355f66585c0c84d03e453741dd3706bea5cd
Pat Nakajima <patnakajima@gmail.com>
2009-04-04 18:46:08 -0400

    tried to use :before and :after
    didn't work in Firefox

--- js/pretty-buttons.js
+++ js/pretty-buttons.js
@@ -1,11 +1,7 @@
 (function($) {
   $(document).ready(function() {
     $('a.button').wrapInner('<span></span>').prepend(
-      '<b class="gradient"></b>' +
-      '<i class="top right"></i>' + 
-      '<i class="top left"></i>' +
-      '<i class="bottom right"></i>' +
-      '<i class="bottom left"></i>'
+      '<b class="gradient"></b>'
     )
   });
 })(jQuery);
