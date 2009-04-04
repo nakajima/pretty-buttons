8906971d536f592c3951f4a830b1ee28df0bff8c
Pat Nakajima <patnakajima@gmail.com>
2009-04-04 18:47:24 -0400

    whitespace

--- js/pretty-buttons.js
+++ js/pretty-buttons.js
@@ -2,7 +2,7 @@
   $(document).ready(function() {
     $('a.button').wrapInner('<span></span>').prepend(
       '<b class="gradient"></b>' +
-      '<i class="top right"></i>' + 
+      '<i class="top right"></i>' +
       '<i class="top left"></i>' +
       '<i class="bottom right"></i>' +
       '<i class="bottom left"></i>'
